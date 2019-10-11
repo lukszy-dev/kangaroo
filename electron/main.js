8fc94cd0b0f8911bbd7a72a8deaaba97cb03779c
Łukasz Szypliński <lukasz.szyplinski@leovegas.com>
2019-10-11 11:23:28 +0200

    Google Drive API push notifications.

--- electron/main.js
+++ electron/main.js
@@ -5,6 +5,7 @@ const { google } = require('googleapis');
 
 const path = require('path');
 const isDev = require('electron-is-dev');
+const uuidv4 = require('uuid/v4');
 
 const { getMainWindow, setMainWindow } = require('./window');
 const { registerListeners, removeListeners } = require('./src/db/db');
@@ -81,7 +82,10 @@ const createAuthClient = () => {
   authClient = new ElectronGoogleOAuth2(
     '25976822649-itstpvr0i60jsmr14oprhej6dsapd9sg.apps.googleusercontent.com',
     '0wD7YYcOwqDTyI5snIF3-_yV',
-    ['https://www.googleapis.com/auth/drive.metadata.readonly']
+    [
+      'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'https://www.googleapis.com/auth/drive.file'
+    ]
   );
 };
 
@@ -125,14 +129,34 @@ ipcMain.on('AUTH_LOGIN', () => {
     const FILE_NAME = 'sm_data';
     const query = "name='" + FILE_NAME + "'";
 
-    drive.files.list({ q: query }).then((response) => {
-      console.log(response.data);
-      if (response.data.files[0].name === FILE_NAME) {
-        console.log('Load data!');
-      } else {
-        drive.files.create({ name: FILE_NAME });
-      }
-    })
+    drive.files.list({ q: query })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.files[0] && response.data.files[0].name === FILE_NAME) {
+          console.log('Load data!');
+        } else {
+          const fileMetadata = {
+            name: FILE_NAME,
+            mimeType: 'text/plain'
+          };
+          drive.files.create({ resource: fileMetadata }).then((response) => {
+            console.log(response);
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    const payload = {
+      type: 'web_hook',
+      id: uuidv4(),
+      address: 'https://proggramik.com/functions/sendNotification'
+    };
+
+    drive.files.watch({ fileId: '18m8tITd-aSTCn6ipj2irrA5O5H0ZwmV-', requestBody: payload }).then((response) => {
+      console.log(response);
+    });
   } else {
     authClient.openAuthWindowAndGetTokens().then(token => {
       store.set('auth_token', token);
